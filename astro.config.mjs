46c3ad0931e6709012ffb9aad86b9b869101dac9
齊田真澄 <masumi-saita@masumiMacBook-Pro.local>
2025-05-06 23:55:46 +0900

    ✨ #5 ポートフォリオ作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://saita0000.github.io',
     base: 'saita0000.github.io',
+    integrations: [preact()],
 })
